773262437390a02dd58188c47519c56a9a658776
ZenNE <eliujl@gmail.com>
2023-05-07 13:00:56 -0500

    Update simpQA_streamlit.py

--- simpQA_streamlit.py
+++ simpQA_streamlit.py
@@ -43,25 +43,28 @@ def query(index, query_text):
     return response
 
 
-# Initialize chatbot history
-chatbot = []
+os.environ['OPENAI_API_KEY'] = st.text_input(
+    "Enter your OpenAI API key", type="password")
 
-# Display file upload component
-files = st.file_uploader('Upload Files', accept_multiple_files=True)
-if files is not None:
-    save_file(files)
+if os.environ['OPENAI_API_KEY']:
+    # Initialize chatbot history
+    chatbot = []
 
+    # Display file upload component
+    files = st.file_uploader('Upload Files', accept_multiple_files=True)
+    if files is not None:
+        save_file(files)
 
-index = ingest('tmp_docs')
+    index = ingest('tmp_docs')
 
-# Display message input component
-message = st.text_input('Enter message')
+    # Display message input component
+    message = st.text_input('Enter message')
 
-# If message is entered, ingest documents and get chatbot response
-if message:
-    chatbot.append(('You', message))
-    chatbot += get_answer(index, message)
+    # If message is entered, ingest documents and get chatbot response
+    if message:
+        chatbot.append(('You', message))
+        chatbot += get_answer(index, message)
 
-# Display chat history
-st.text_area('Chatbot:', value='\n'.join(
-    [f'{x[0]}: {x[1]}' for x in chatbot]), height=250)
+    # Display chat history
+    st.text_area('Chatbot:', value='\n'.join(
+        [f'{x[0]}: {x[1]}' for x in chatbot]), height=250)
